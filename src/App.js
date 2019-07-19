7fff6169dec2c37e295eb179776803da3ab46017
NemanjaKusljic <nemanja.kuslja@gmail.com>
2019-07-19 13:24:35 +0200

    Rendering content conditionally

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       {name:'Manu', age: 27 },
       {name:'Marina', age: 21 },
       {name:'Siki', age: 37 }
-    ]
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -18,23 +19,54 @@ class App extends Component {
         {name:'Marina', age: 21 },
         {name:'Piki', age: 30 }
       ]
+
     })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState( {
+      persons: [
+        {name:'Tramp', age: 27 },
+        {name:event.target.value, age: 21 },
+        {name:'Piki', age: 30 }
+      ]
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow });
+  }
+
+  
+
   render() {
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            change={this.nameChangeHandler}
+            click ={this.switchNameHandler.bind(this, 'Petar')} /*added property click that references to method*/>Moji hobiji: spavanje</Person> 
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+          </div>
+      )
+    };
+
     return (
       <div className="App">
-        <button onClick={() => this.switchNameHandler('Maxim')}>Swith name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click ={this.switchNameHandler.bind(this, 'Petar')} /*added property click that references to method*/>Moji hobiji: spavanje</Person> 
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
+        <button onClick={this.togglePersonsHandler}>Swith name</button>
+        {persons}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => { //when using class based components we put this.props.
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>My name is {props.name} and I'm {props.age} years old.</p> 
             <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name}></input>
         </div>
     )
 };
